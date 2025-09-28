6fd5962916da9a0d508db00b420d926815d22d32
shivaraj245 <shivarajsmanikashetti7323@gmail.com>
2025-09-28 22:53:01 +0530

    Upgrade AWS SDK from v2 to v3 - Remove maintenance mode warnings and improve performance

--- back/s3.js
+++ back/s3.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv'
-import aws from 'aws-sdk'
+import { S3Client } from '@aws-sdk/client-s3'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import { PutObjectCommand } from '@aws-sdk/client-s3'
 import crypto from 'crypto'
 import { promisify } from "util"
 const randomBytes = promisify(crypto.randomBytes)
@@ -11,23 +13,23 @@ const bucketName = "shivaraj-uploadfile"
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
 
-const s3 = new aws.S3({
+const s3 = new S3Client({
   region,
-  accessKeyId,
-  secretAccessKey,
-  signatureVersion: 'v4'
+  credentials: {
+    accessKeyId,
+    secretAccessKey
+  }
 })
 
 export async function generateUploadURL() {
   const rawBytes = await randomBytes(16)
   const imageName = rawBytes.toString('hex')
 
-  const params = ({
+  const command = new PutObjectCommand({
     Bucket: bucketName,
-    Key: imageName,
-    Expires: 60
+    Key: imageName
   })
   
-  const uploadURL = await s3.getSignedUrlPromise('putObject', params)
+  const uploadURL = await getSignedUrl(s3, command, { expiresIn: 60 })
   return uploadURL
 }
